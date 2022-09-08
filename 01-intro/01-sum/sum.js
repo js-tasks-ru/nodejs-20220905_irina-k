c144e22bb606db7ea82488c591e918ab1660ea3a
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2022-09-08 14:50:23 +0300

    sum task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,23 @@
+/**
+ * Функция проверки, является ли аргумент числом.
+ * @param arg {*} Аргумент, который проверяется на тип.
+ * @returns {boolean}
+ */
+function isNumber(arg) {
+	return typeof arg === 'number' && !isNaN(arg);
+}
+
+/**
+ * Функция возврата суммы двух чисел.
+ * @param a {number} Первый аргумент для суммы.
+ * @param b {number} Второй аргумент для суммы.
+ * @returns {number}
+ */
 function sum(a, b) {
-  /* ваш код */
+  if (isNumber(a) && isNumber(b)) return a + b;
+	else {
+		throw new TypeError('Аргументы не являются числами');
+	}
 }
 
 module.exports = sum;
