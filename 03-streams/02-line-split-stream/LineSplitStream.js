1941e5425d1b60cc9e959c913fccda1d646a8752
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2022-10-10 17:50:23 +0300

    added split limit task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,42 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.remainder = '';
+    this.chunkList = [];
+  }
+
+  modifyRemainder(mergedData) {
+    this.remainder = mergedData;
   }
 
   _transform(chunk, encoding, callback) {
+    /** Приводим чанк к строке. */
+    const stringChunk = chunk.toString();
+    /** Находим последнюю позицию символа переноса. */
+    const chunkIndex = stringChunk.lastIndexOf(os.EOL);
+    /** Разбиваем чанк по символу переноса. */
+    const splitData = stringChunk.split(os.EOL);
+    splitData.forEach((p, index) => {
+      /** Склеиваем первую часть нынешнего чанка и последнюю часть предыдущего. */
+      const mergedData = (this.chunkList.length && !index) ? `${this.chunkList[this.chunkList.length - 1]}${p}` : p;
+      /** Сохраняем в общее хранилище. */
+      this.chunkList.push(p);
+      /** Если в чанке нет символа переноса, сохраняем 'скленные' данные на всякий случай в хранилище для окончательного вывода. */
+      if (chunkIndex === -1) {
+        this.modifyRemainder(mergedData);
+        return;
+      }
+      if (index !== splitData.length - 1) {
+        this.push(mergedData);
+      } else {
+        this.modifyRemainder(mergedData);
+      }
+    });
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.remainder);
   }
 }
 
