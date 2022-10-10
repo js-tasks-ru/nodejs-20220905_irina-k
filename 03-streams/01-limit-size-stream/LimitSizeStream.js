bf310e83770a693b22f90b0c942a1709a9fbf8fc
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2022-10-10 18:04:10 +0300

    corrected size limit stream task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,12 +1,13 @@
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
-const chunksList = [];
+// const chunksList = [];
 
 class LimitSizeStream extends stream.Transform {
 	constructor(options) {
 		super(options);
 		this.options = options;
+		this.chunksList = [];
 	}
 
   /** Функция преобразования. */
@@ -14,14 +15,14 @@ class LimitSizeStream extends stream.Transform {
     /** Опредедяем размер чанка.  */
 		const chunkSize = Buffer.byteLength(chunk.toString());
     /** Определяем размер предыдущего чанка. */
-    const previousChunkSize = chunksList.length ? chunksList.reduce((prev, current) => prev + current, 0) : 0;
+    const previousChunkSize = this.chunksList.length ? this.chunksList.reduce((prev, current) => prev + current, 0) : 0;
     /** Определяем размер обновленного чанка. */
 		const updatedChunkSize = previousChunkSize + chunkSize;
     /** Проверяем на соответствие лимиту. */
 		if (updatedChunkSize > this.options.limit) {
 			callback(new LimitExceededError(), chunk);
 		} else {
-			chunksList.push(chunkSize);
+			this.chunksList.push(chunkSize);
 			callback(null, chunk);
 		}
 	}
