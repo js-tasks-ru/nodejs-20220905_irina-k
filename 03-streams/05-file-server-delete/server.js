1cf85ced4614c4ef9d1b8d4bb6bd19d4cc6f42f5
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2022-10-23 01:13:50 +0300

    added file server delete task

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,7 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
@@ -12,7 +12,31 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
-
+      /** Проверяем корректность маршрута. */
+      const lastIndex = pathname.lastIndexOf('/');
+      if (lastIndex !== -1 && lastIndex !== 0) {
+        res.statusCode = 400;
+        res.end('Incorrect path');
+        return;
+      } else {
+        /** Проверяем, присутствует ли файл. */
+        if (!fs.existsSync(filepath)) {
+          res.statusCode = 404;
+          res.end('No file');
+          return;
+        } else {
+          /** Удаляем файл. */
+          fs.unlink(filepath, (error) => {
+            if (error) {
+              res.statusCode = 500;
+              res.end('Strange Error');
+            } else {
+              res.end('Your file was deleted');
+            }
+            return;
+          });
+        }
+      }
       break;
 
     default:
