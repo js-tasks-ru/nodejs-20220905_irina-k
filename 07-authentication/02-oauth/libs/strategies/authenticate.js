b4d41d47abbc723f7dd88cc3ef9288a7e6f3ab43
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2022-12-11 00:31:28 +0300

    07-authentication-02

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,19 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, `Не указан email`);
+  }
+  const user = await User.findOne({email});
+  if (user) {
+    return done(null, user);
+  }
+
+  try {
+    const createdUser = new User({ email, displayName });
+    await createdUser.save();
+    done(null, createdUser);
+  } catch (er) {
+    done(er);
+  }
 };
