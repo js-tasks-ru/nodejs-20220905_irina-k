e43a58c879b9be099044d1947a58386a62f0c77f
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2022-12-12 03:02:28 +0300

    08-email-01-registration

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -3,9 +3,36 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
-
+  const { email, displayName, password } = ctx.request.body;
+	const token = uuid();
+	try {
+		const newUser = new User({ email, displayName, verificationToken: token });
+		await newUser.setPassword(password);
+		await newUser.save();
+    await sendMail({
+			template: 'confirmation',
+			locals: { token },
+			to: email,
+			subject: 'Подтвердите почту',
+		});
+		ctx.body = { status: 'ok' };
+	} catch (err) {
+		throw(err);
+	}
 };
 
 module.exports.confirm = async (ctx, next) => {
-
+	const { verificationToken } = ctx.request.body;
+	try {
+		const user = await User.findOne({ verificationToken });
+		if (!user) {
+			ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+			return;
+		}
+		await User.updateOne({ verificationToken }, { $unset: { verificationToken } });
+		const token = await ctx.login(user);
+		ctx.body = { token };
+	} catch (err) {
+		throw err;
+	}
 };
