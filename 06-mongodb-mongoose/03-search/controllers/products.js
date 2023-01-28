5bce9af66030ddd0c5df3e2cac55f88bb3e12e78
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2023-01-29 00:53:20 +0300

    added 06-mongodb-mongoose-03-search

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,29 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
+const createProductsList = (res) => {
+  const list = res.map((p) => mapProduct(p));
+  return list;
+}
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
-};
+const {query} = ctx.query;
+if (!query) {
+  await Product.find({}).then((res) => {
+    if (res.length) {
+      const products = createProductsList(res);
+      ctx.body = {products};
+    } else {
+      ctx.body = {products: []};
+    }
+  })
+} else if (query) {
+  await Product.find({$text: {$search: query}}).then((res) => {
+    if (res.length) {
+      const products = createProductsList(res);
+      ctx.body = {products};
+    } else {
+      ctx.body = {products: []};
+    }
+  });
+}};
