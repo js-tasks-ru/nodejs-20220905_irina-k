6d1bf9a42b47dc70c33f9a53ce850100d8ce15ff
Irina Kurdyukova <irina.kurdyukova@givc.mkrf.ru>
2023-01-28 22:29:28 +0300

    added 09-websockets-01-chat

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,14 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  const {user} = ctx;
+  const {id} = user;
+  try {
+    const messages = await Message.find({chat: id}).limit(20);
+    const updatedMessages = messages.map(mapMessage);
+    ctx.body = {messages: updatedMessages};
+  } catch (err) {
+    throw err;
+  }
 };
